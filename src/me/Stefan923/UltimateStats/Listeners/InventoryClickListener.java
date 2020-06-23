52df48b99bdd9477d9099d9ccd1c823e5ca964f7
Stefan923 <stefanpopescu923@gmail.com>
2020-06-23 16:22:56 +0300

    Added a listener for modifing inventories.

---
+++ src/me/Stefan923/UltimateStats/Listeners/InventoryClickListener.java
@@ -0,0 +1,39 @@
+package me.Stefan923.UltimateStats.Listeners;
+
+import me.Stefan923.UltimateStats.UltimateStats;
+import me.Stefan923.UltimateStats.Utils.User;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
+
+public class InventoryClickListener implements Listener {
+    UltimateStats instance;
+
+    public InventoryClickListener(UltimateStats instance) {
+        this.instance = instance;
+    }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        Player player = (Player) event.getWhoClicked();
+        User user = instance.getUser(player);
+        if (!user.isWatching()) {
+            return;
+        }
+
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onInventoryDrag(InventoryDragEvent event) {
+        Player player = (Player) event.getWhoClicked();
+        User user = instance.getUser(player);
+        if (!user.isWatching()) {
+            return;
+        }
+
+        event.setCancelled(true);
+    }
+}
