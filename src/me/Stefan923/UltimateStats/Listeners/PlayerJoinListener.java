e052b2e11bb7dd1eff5f89edb2608a5f0abfaf59
BrowkS <stefanpopescu923@gmail.com>
2020-06-28 11:51:47 +0300

    Added player quit/join listeners.

---
+++ src/me/Stefan923/UltimateStats/Listeners/PlayerJoinListener.java
@@ -0,0 +1,17 @@
+package me.Stefan923.UltimateStats.Listeners;
+
+import me.Stefan923.UltimateStats.UltimateStats;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class PlayerJoinListener implements Listener {
+    private UltimateStats instance;
+
+    public PlayerJoinListener(UltimateStats instance) {
+        this.instance = instance;
+    }
+
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        instance.addUser(event.getPlayer());
+    }
+}
---
+++ src/me/Stefan923/UltimateStats/Listeners/PlayerQuitListener.java
@@ -0,0 +1,16 @@
+package me.Stefan923.UltimateStats.Listeners;
+
+import me.Stefan923.UltimateStats.UltimateStats;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class PlayerQuitListener {
+    private UltimateStats instance;
+
+    public PlayerQuitListener(UltimateStats instance) {
+        this.instance = instance;
+    }
+
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        instance.removeUser(event.getPlayer());
+    }
+}
