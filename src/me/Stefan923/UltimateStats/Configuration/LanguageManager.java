ad426ec69293203953c93505b691df491f96b29e
Stefan923 <stefanpopescu923@gmail.com>
2020-07-14 10:59:11 +0300

    Added a message for wrong command syntax.

--- src/me/Stefan923/UltimateStats/Configuration/LanguageManager.java
+++ src/me/Stefan923/UltimateStats/Configuration/LanguageManager.java
@@ -8,7 +8,6 @@ import org.bukkit.plugin.Plugin;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 
 public class LanguageManager implements MessageUtils {
 
@@ -41,6 +40,10 @@ public class LanguageManager implements MessageUtils {
 
         config = YamlConfiguration.loadConfiguration(cfile);
         config.options().header("UltimateStats by Stefan923.\n");
+        config.addDefault("General.Invalid Command Syntax", "&8(&3!&8) &cInvalid Syntax or you have no permission!\n&8(&3!&8) &fThe valid syntax is: &b%syntax%");
+        config.addDefault("General.Must Be Online", "&8(&3!&8) &cSpecified player must be online!");
+        config.addDefault("General.Must Be Player", "&8(&3!&8) &cYou must be a player to do this!");
+        config.addDefault("General.No Permission", "&8(&3!&8) &cYou need the &4%permission% &cpermission to do that!");
         config.addDefault("Update Checker.Available", "&8(&3!&8) &fThere is a new version of &bUltimateStats &favailable!\n&8(&3!&8) &fDownload link: &b%link%");
         config.addDefault("Update Checker.Not Available", "&8(&3!&8) &fThere's no update available for &bUltimateStats&f.");
         config.options().copyDefaults(true);
